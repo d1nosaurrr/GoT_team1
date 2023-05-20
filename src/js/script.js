a844f90eb66340b1def61f2753bcb63eb71c71f2
d1nosaur <bykoua@gmail.com>
2023-05-20 21:24:35 +0300

    update styles

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,13 @@ window.addEventListener('scroll', () => {
     :
     headerFixed.classList.remove('scroll');
 });
+
+const dropdown = document.querySelector('.header__dropdown');
+const filterBlock = document.querySelector('.filter__block');
+let isOpen = false;
+dropdown.addEventListener('click', () => {
+  isOpen ? filterBlock.classList.remove('active') : filterBlock.classList.add('active');
+  isOpen ? dropdown.style.transform = 'rotate(0)'
+    : dropdown.style.transform = 'rotate(180deg)';
+  isOpen = !isOpen;
+});
