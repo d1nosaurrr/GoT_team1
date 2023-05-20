74012b47dcbe7bb730a3e505f37f9b91c71f2637
Elena_Sloth <ymnutsa@yandex.ua>
2023-05-20 23:45:20 +0300

    card effect and fixed a bug
    card effect and fixed a bug

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 window.addEventListener('scroll', () => {
   let headerFixed = document.querySelector('.header__fixed');
-  document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ?
-    headerFixed.classList.add('scroll')
-    :
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+    if(document.querySelector('.container').clientHeight < 560){
+      let mainNew = document.querySelector('.main');
+      mainNew.classList.add("mainNew");
+    }
+    headerFixed.classList.add('scroll');
+  }
+   else{
+    if(document.querySelector('.container').clientHeight > 560){
+      let mainNew = document.querySelector('.main');
+      mainNew.classList.remove("mainNew");
+    }
     headerFixed.classList.remove('scroll');
-});
+   }
+})
